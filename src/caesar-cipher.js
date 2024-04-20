9a9b5fe0dc74ed63997f1aa0a96e18e90dec319c
Bodi_Ali <bodiali59@gmail.com>
2024-04-20 12:46:30 +0200

    Add caesar cipher module
    A caesar cipher is an alphabet cipher which shifts
    the given character by 3 characters,
    That is, "a" was encrypted as "D", "b" as "E", etc.

---
+++ src/caesar-cipher.js
@@ -0,0 +1,101 @@
+function cipherString(str) {
+  const plainText = [
+    " ",
+    ".",
+    ",",
+    "?",
+    "!",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+  ];
+  const cipherText = [
+    "?",
+    "!",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    " ",
+    ".",
+    ",",
+  ];
+  const arr = str.split("").map((char) => {
+    const index = plainText.indexOf(char);
+    if (index !== -1) {
+      return cipherText[index];
+    }
+    return char;
+  });
+  return arr.join("");
+}
+
+export default function caesarCipher(str) {
+  const lowerStr = str.toLowerCase();
+  return cipherString(lowerStr);
+}
--- src/test.js
+++ src/test.js
@@ -1,6 +1,8 @@
 import capitalize from "./capitalize.js";
 import reverseString from "./reverse-string.js";
 import calculator from "./calculator.js";
+import caesarCipher from "./caesar-cipher.js";
+import analyzeArray from "./analyze-array.js";
 
 // Capitalize test
 test("Capitalize", () => {
@@ -41,4 +43,28 @@ test("Calculator object: Multiplication", () => {
   expect(calculator.multiply(2, 4)).toBe(8);
 });
 
-// caesarCipher test
+// Caesar Cipher test
+test("Caesar Cipher: abc becomes DEF", () => {
+  expect(caesarCipher("abc")).toBe("DEF");
+});
+
+test("Caesar Cipher: my name is Bodi becomes P1?QDPH?LV?ERGL", () => {
+  expect(caesarCipher("my name is Bodi")).toBe("P1?QDPH?LV?ERGL");
+});
+
+test("Caesar Cipher: HEY!!! becomes KH1CCC", () => {
+  expect(caesarCipher("HEY!!!")).toBe("KH1CCC");
+});
+
+test("Caesar Cipher: '012,. ' becomes 345A!?", () => {
+  expect(caesarCipher("012,. ")).toBe("345A!?");
+});
+
+// Analyze Array test
+test("Analyze Array: Floating-point number", () => {
+  expect(analyzeArray([10, 2, 6, 3, 6, 5])).toEqual({ average: 5.33, min: 2, max: 10, length: 6 });
+});
+
+test("Analyze Array", () => {
+  expect(analyzeArray([2, 0, 3, 54, 6, 11, 1])).toEqual({ min: 0, max: 54, average: 11, length: 7 });
+});
